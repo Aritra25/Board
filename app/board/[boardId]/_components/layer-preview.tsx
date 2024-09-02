b9a82a7c301bdb8078fc4449f2b4adae2ff39e8a
Aritra25 <aritra.maji2001@gmail.com>
2024-09-02 13:23:37 +0530

    plss

--- app/board/[boardId]/_components/layer-preview.tsx
+++ app/board/[boardId]/_components/layer-preview.tsx
@@ -73,7 +73,7 @@ export const LayerPreview = memo(
           />
         );
       default:
-        console.warn("Unknown layer type:", layer.type);
+        // console.warn("Unknown layer type:", layer.type);
         return null;
     }
   },
